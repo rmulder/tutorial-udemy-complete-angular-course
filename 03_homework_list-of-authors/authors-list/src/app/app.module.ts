06f3997f80c7fe024b1f70f8e8dcaf62e223a125
Manuel <7874524+bertoluzza@users.noreply.github.com>
2019-08-06 11:18:23 +0200

    section 3 homework 1

---
+++ 03_homework_list-of-authors/authors-list/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { AuthorsService } from './authors.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AuthorsComponent } from './authors/authors.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthorsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [AuthorsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 03_homework_list-of-authors/authors-list/src/app/authors/authors.component.ts
@@ -0,0 +1,23 @@
+import { AuthorsService } from './../authors.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+})
+export class AuthorsComponent implements OnInit {
+  authors: string[];
+  
+  constructor(AuthorsService: AuthorsService) {
+    this.authors = AuthorsService.getAuthors();
+   }
+
+  ngOnInit() {
+  }
+
+  getNumAuthors(){
+    return this.authors.length;
+  }
+
+}
